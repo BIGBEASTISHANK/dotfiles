e7e9b354c90db8477132583625afd5ca92aa1b0c
BIGBEASTISHANK <ishank@bigbeastishank.com>
2025-06-05 21:46:27 +0530

    [UPDATE] OMF Bira theme in `config.fish`

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,64 @@
+# Commands to run in interactive sessions can go here
 if status is-interactive
-    # Commands to run in interactive sessions can go here
     fastfetch
+    # echo "------------------------------------------------------------------"
+    # cal -3
+    # echo "------------------------------------------------------------------"
 end
 
 set fish_greeting
+
+# Theme
+# Theme stolen from bira of omf
+function __user_host
+  set -l content 
+  if [ (id -u) = "0" ];
+    echo -n (set_color --bold red)
+  else
+    echo -n (set_color --bold green)
+  end
+  echo -n $USER@(uname -n) (set color normal)
+end
+
+function __current_path
+  echo -n (set_color --bold blue) (pwd) (set_color normal) 
+end
+
+function _git_branch_name
+  echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
+end
+
+function _git_is_dirty
+  echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
+end
+
+function __git_status
+  if [ (_git_branch_name) ]
+    set -l git_branch (_git_branch_name)
+
+    if [ (_git_is_dirty) ]
+      set git_info '<'$git_branch"*"'>'
+    else
+      set git_info '<'$git_branch'>'
+    end
+
+    echo -n (set_color red) $git_info (set_color normal) 
+  end
+end
+
+function fish_prompt
+  echo -n (set_color white)"╭─"(set_color normal)
+  __user_host
+  __current_path
+  __git_status
+  echo -e ''
+  echo (set_color white)"╰─"(set_color --bold white)"\$ "(set_color normal)
+end
+
+function fish_right_prompt
+  set -l st $status
+
+  if [ $st != 0 ];
+    echo (set_color red) ↵ $st(set_color normal)
+  end
+end
